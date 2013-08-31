2b2531b25bf94132be4114c511599f75975dc370
tusha <katja.safronova@gmail.com>
2013-08-31 13:41:51 +0200

    second draft

--- Gemfile
+++ Gemfile
@@ -105,6 +105,7 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'meta_request'
+  gem 'capistrano'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,68 @@
+set :application, "lu.uni.hpc"
+set :scm_username, "passenger"
+set :repository,  "https://github.com/tusha/lu.uni.hpc.git"
+set :git_enable_submodules, 1
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :branch, "master"
+set :user, "localadmin"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink shared resources on each release"
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
+
+namespace :bundler do
+  desc "Symlink bundled gems on each release"
+  task :symlink_bundled_gems, :roles => :app do
+    run "mkdir -p #{shared_path}/bundled_gems"
+    run "ln -nfs #{shared_path}/bundled_gems #{release_path}/vendor/bundle"
+  end
+
+  desc "Install for production"
+  task :install, :roles => :app do
+    run "cd #{release_path} && bundle install --production"
+  end
+
+
+end
+
+after 'deploy:update_code', 'bundler:symlink_bundled_gems'
+# after 'deploy:update_code', 'bundler:install'
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
